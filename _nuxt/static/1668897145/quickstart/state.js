9b84a42f322c66b3797c356032cad0878777000a
-DigitalMaster- <112659665+TheRealDigitalMaster@users.noreply.github.com>
2022-11-30 16:41:04 -0500

    Create state.js

---
+++ _nuxt/static/1668897145/quickstart/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1668897145",layout:"default",error:a,serverRendered:b,routePath:"\u002Fquickstart",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0f5fe3d1"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"light"));
